843ab9a0fda5b0472289c3b4800f8f7bbdc5036e
Fawwaz Firdaus <fawwazfirdaus7@gmail.com>
2025-08-29 23:53:55 -0700

    Add description for Iris in ContentView.swift

--- Iris/ContentView.swift
+++ Iris/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, Iris!")
+            Text("Iris is an invisible companion that assists you in your daily life.")
         }
         .padding()
     }
