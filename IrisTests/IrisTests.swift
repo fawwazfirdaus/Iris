5ea37c28a5d5b60cb5d7c597fddc3f0254b9812d
Fawwaz Firdaus <fawwazfirdaus7@gmail.com>
2025-08-29 23:42:42 -0700

    Initial commit: Iris SwiftUI project

---
+++ IrisTests/IrisTests.swift
@@ -0,0 +1,17 @@
+//
+//  IrisTests.swift
+//  IrisTests
+//
+//  Created by Fawwaz Firdaus on 8/29/25.
+//
+
+import Testing
+@testable import Iris
+
+struct IrisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
